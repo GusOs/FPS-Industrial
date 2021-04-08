6a38194a7a5a5ed0bebdae8b912f2c235deb2f03
GusOs <gus_92@hotmail.es>
2021-04-08 15:58:52 +0200

    Add spawn item

---
+++ FPS-Industrial/Assets/Scripts/ItemSpawnLife.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnLife : MonoBehaviour
+{
+
+    //Gameobject del item de vida
+    public GameObject[] itemLife;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnItem(); 
+    }
+
+    public void SpawnItem()
+    {
+        Instantiate(itemLife[Random.Range(0, itemLife.Length)], transform.position, Quaternion.identity);
+    }
+}
